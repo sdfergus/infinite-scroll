c9ac6890e802b706bf7cc16df0f37d591df50d97
Shandira Ferguson <shandira.ferguson@gmail.com>
2023-08-07 19:31:01 -0700

    Unsplash API setup + photoDisplay function added

---
+++ script.js
@@ -0,0 +1,45 @@
+const imageContainer = document.getElementById('img-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
+//Unsplash API
+const count = 10;
+const apiKey = 'MlgAA905b1qqyYhzEjmFm8h1DUehjQXS703pUV9qy20';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+// const apiUrl = `https://api.unsplash.com/search/collections?page=1&query=motivational_quotes&client_id=${apiKey}&count=${count}`;
+
+//Create Elements for Links & Photos, Add to DOM
+function displayPhotos() {
+    photosArray.forEach(photo => {
+        //Create <a> to link to Unsplash
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+        //Create <img> for photo
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
+        //Put <img> inside <a>, then put both inside imageContainer Element
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });  
+}
+
+
+//Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        // console.log(photosArray);
+        displayPhotos();
+    } catch (error) {
+        //Catch error here
+        console.log('Whoops! Error found: ', error);
+    }
+}
+
+//On load
+getPhotos();
